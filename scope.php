3454fa2ea645b6387d7f531b2195435be9f82e81
Leeso68 <109051958+Leeso68@users.noreply.github.com>
2022-08-17 12:15:37 +0900

    Add files via upload

---
+++ scope.php
@@ -0,0 +1,15 @@
+<?php
+function counts()
+{
+    $i = 0;         //local variable
+    $i = $i + 10;
+    global $g;
+    $g++;
+}
+$g = 0;             //local variable
+while ($g < 10) {
+    counts();
+    print $g . "<br>";
+}
+
+?>
---
+++ swap.php
@@ -0,0 +1,27 @@
+<?php
+function swap01($a, $b)     //call by value
+{
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
+}
+$i = 1;
+$j = 6;
+
+print "\$i = $i, \$j = $j <br>";
+swap01($i, $j);
+print "\$i = $i, \$j = $j <br><hr>";
+function swap02(&$a, &$b)       //call by value
+                                //주소에 있는 값을 처리
+{
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
+}
+$i = 1;
+$j = 6;
+
+print "\$i = $i, \$j = $j <br>";
+swap02($i, $j);
+print "\$i = $i, \$j = $j <br>";
+?>
